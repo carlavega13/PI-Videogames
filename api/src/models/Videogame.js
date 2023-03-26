bb6e9d2e1558b44950b7cc0e4bd92cb942174ad3
carlavega13 <carlavega231323@gmail.com>
2023-03-26 02:09:08 -0300

    MODELS DDB

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,10 +1,36 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    plataforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
       type: DataTypes.STRING,
       allowNull: false,
     },
